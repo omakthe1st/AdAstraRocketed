6fa60b66f9e26e6a253433523ae83e6e7e777daa
celsiusqc <63996164+celsiusqc@users.noreply.github.com>
2024-04-03 06:52:31 +0200

    Fixed custom model bug 1.0.1

--- common/src/main/java/net/celsiusqc/ad_astra_rocketed/client/renderers/entities/AdAstraRocketedRenderer.java
+++ common/src/main/java/net/celsiusqc/ad_astra_rocketed/client/renderers/entities/AdAstraRocketedRenderer.java
@@ -5,6 +5,6 @@ import net.minecraft.resources.ResourceLocation;
 
 public class AdAstraRocketedRenderer {
     public static final ResourceLocation TIER_5_TEXTURE = new ResourceLocation(AdAstraRocketed.MOD_ID, "textures/entity/rocket/tier_5_rocket.png");
-    public static final ResourceLocation TIER_6_TEXTURE = new ResourceLocation(AdAstraRocketed.MOD_ID, "textures/entity/rocket/tier_6_rocket.png");
+    public static final ResourceLocation TIER_6_TEXTURE = new ResourceLocation(AdAstraRocketed.MOD_ID, "textures/entity/rocket/tier_5_rocket.png");
     public static final ResourceLocation TIER_7_TEXTURE = new ResourceLocation(AdAstraRocketed.MOD_ID, "textures/entity/rocket/tier_7_rocket.png");
 }
